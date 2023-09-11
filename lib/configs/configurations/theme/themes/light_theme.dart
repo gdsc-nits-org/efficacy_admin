4af4472edbcee367188afa997dc3c6f7c5c92f7a
ARUNAV BORTHAKUR <122561140+Arunav47@users.noreply.github.com>
2023-09-11 23:52:23 +0530

    button styles created and added a color in palette

--- lib/configs/configurations/theme/themes/light_theme.dart
+++ lib/configs/configurations/theme/themes/light_theme.dart
@@ -14,13 +14,17 @@ ThemeData lightTheme = ThemeData(
   useMaterial3: true,
   iconTheme: const IconThemeData(color: accent),
   floatingActionButtonTheme: FloatingActionButtonThemeData(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+    backgroundColor: dark,
+    foregroundColor: light,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
   ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ElevatedButton.styleFrom(
       elevation: 2,
+      minimumSize: const Size(264, 58),
       backgroundColor: dark,
       foregroundColor: light,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9)),
     ),
   ),
   iconButtonTheme: IconButtonThemeData(
@@ -28,7 +32,20 @@ ThemeData lightTheme = ThemeData(
       elevation: 4,
       backgroundColor: dark,
       foregroundColor: light,
+
     ),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: dark,
+      foregroundColor: light,
+    )
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: light,
+      shadowColor: shadow,
+    )
   ),
   appBarTheme: const AppBarTheme(
     backgroundColor: light,

03f2da389cef50e8660ada475ce87b337579dcf3
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2023-09-23 21:01:16 +0530

    added AppHomepage route to routes

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       routes: {
         AppHomepage.routeName: (context) => const AppHomepage(),

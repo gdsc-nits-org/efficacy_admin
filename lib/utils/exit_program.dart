8aa2efbdb74b5b0f52de194a81d8f671b7bd3185
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2023-10-12 16:12:44 +0530

    const added

--- lib/utils/exit_program.dart
+++ lib/utils/exit_program.dart
@@ -7,14 +7,14 @@ Future<bool?> showExitWarning(BuildContext context) async {
     showDialog<bool>(
         context: context,
         builder: (context) => AlertDialog(
-              title: Text("Do you want to exit the app?"),
+              title: const Text("Do you want to exit the app?"),
               actions: [
                 TextButton(
                     onPressed: () => Navigator.pop(context, false),
-                    child: Text("No")),
+                    child: const Text("No")),
                 TextButton(
                     onPressed: () => Navigator.pop(context, true),
-                    child: Text("Yes"))
+                    child: const Text("Yes"))
               ],
               alignment: Alignment.center,
               actionsAlignment: MainAxisAlignment.spaceEvenly,

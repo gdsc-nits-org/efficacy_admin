764508be53317ee2d529dbd73e5950bb61a69834
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2023-11-01 02:31:12 +0530

    fix

--- lib/pages/splash_screen/splash_screen.dart
+++ lib/pages/splash_screen/splash_screen.dart
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
       debugPrint("Time taken: $duration ms");
       Navigator.of(context)
           .pushNamedAndRemoveUntil(
-              ProfilePage.routeName, (Route<dynamic> route) => false)
+              Homepage.routeName, (Route<dynamic> route) => false)
           .then((value) => exit(0));
     });
   }

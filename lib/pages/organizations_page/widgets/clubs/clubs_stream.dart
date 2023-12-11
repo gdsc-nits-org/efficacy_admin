300d4906e535f7939144383652cc0bf0f28d05d7
Arunaditya Dasgupta <adg8503@gmail.com>
2023-12-11 22:01:27 +0530

    commit re-added

--- lib/pages/organizations_page/widgets/clubs/clubs_stream.dart
+++ lib/pages/organizations_page/widgets/clubs/clubs_stream.dart
@@ -1,4 +1,3 @@
-import 'package:efficacy_admin/config/config.dart';
 import 'package:efficacy_admin/controllers/services/services.dart';
 import 'package:efficacy_admin/models/club/club_model.dart';
 import 'package:flutter/material.dart';
@@ -37,44 +36,46 @@ class _ClubsStreamState extends State<ClubsStream> {
                   padding: const EdgeInsets.only(
                       top: ClubsStream.smallPadding,
                       bottom: ClubsStream.smallPadding),
-                  child: ListTile(
-                    contentPadding: EdgeInsets.only(
-                        left: ClubsStream.smallPadding,
-                        right: ClubsStream.largePadding,
-                        top: ClubsStream.largePadding,
-                        bottom: ClubsStream.largePadding),
-                    title: Text(
-                      club.name,
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium
-                          ?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    trailing: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Institute: ${club.instituteName}',
-                          style: Theme.of(context).textTheme.labelMedium,
+                  child: Card(
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.only(
+                          left: ClubsStream.smallPadding,
+                          right: ClubsStream.largePadding,
+                          top: ClubsStream.largePadding,
+                          bottom: ClubsStream.largePadding),
+                      title: Text(
+                        club.name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Institute: ${club.instituteName}',
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                          Text(
+                            'Email: ${club.email}',
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
+                      leading: ClipOval(
+                        child: Image(
+                          image: NetworkImage(club.clubLogoURL),
+                          errorBuilder: (context, error, stackTrace) =>
+                              const Icon(Icons.people),
+                          width: ClubsStream.imageWidth,
+                          // Adjust the width as needed
+                          height: ClubsStream.imageWidth,
+                          // Adjust the height as needed
+                          fit: BoxFit.cover,
                         ),
-                        Text(
-                          'Email: ${club.email}',
-                          style: Theme.of(context).textTheme.labelMedium,
-                        ),
-                      ],
-                    ),
-                    onTap: () {},
-                    leading: ClipOval(
-                      child: Image(
-                        image: NetworkImage(club.clubLogoURL),
-                        errorBuilder: (context, error, stackTrace) =>
-                            const Icon(Icons.people),
-                        width: ClubsStream.imageWidth,
-                        // Adjust the width as needed
-                        height: ClubsStream.imageWidth,
-                        // Adjust the height as needed
-                        fit: BoxFit.cover,
                       ),
                     ),
                   ),

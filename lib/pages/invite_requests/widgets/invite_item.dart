d6618e8e81c0b5b5885d3b489f64dfefd663d674
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-02-12 16:50:25 +0530

    made invite card

---
+++ lib/pages/invite_requests/widgets/invite_item.dart
@@ -0,0 +1,187 @@
+import 'package:efficacy_admin/config/config.dart';
+import 'package:efficacy_admin/controllers/controllers.dart';
+import 'package:efficacy_admin/dialogs/loading_overlay/loading_overlay.dart';
+import 'package:efficacy_admin/models/invitation/invitaion_model.dart';
+import 'package:efficacy_admin/models/models.dart';
+import 'package:efficacy_admin/widgets/profile_image_viewer/profile_image_viewer.dart';
+import 'package:efficacy_admin/widgets/snack_bar/error_snack_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InviteItem extends StatefulWidget {
+  final String clubPositionID;
+  final String receiverID;
+  final InvitationModel invitation;
+  final Function refresh;
+  // final VoidCallback onCompleteAcceptOrReject;
+  const InviteItem({
+    super.key,
+    required this.clubPositionID,
+    required this.receiverID,
+    required this.invitation,
+    required this.refresh,
+    // required this.onCompleteAcceptOrReject,
+  });
+
+  @override
+  State<InviteItem> createState() => _InviteItemState();
+}
+
+class _InviteItemState extends State<InviteItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5.0),
+      decoration: const BoxDecoration(
+          border: BorderDirectional(bottom: BorderSide(color: shadow))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              StreamBuilder<List<ClubPositionModel>>(
+                stream: ClubPositionController.get(
+                    clubPositionID: widget.clubPositionID),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  ClubPositionModel? clubPosition;
+                  if (snapshot.hasData &&
+                      snapshot.data != null &&
+                      snapshot.data.isNotEmpty) {
+                    clubPosition = snapshot.data.first;
+                  }
+                  return StreamBuilder<List<ClubModel>>(
+                    stream: clubPosition?.id != null
+                        ? ClubController.get(id: clubPosition?.clubID)
+                        : null,
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      ClubModel? club;
+                      if (snapshot.hasData &&
+                          snapshot.data != null &&
+                          snapshot.data.isNotEmpty) {
+                        club = snapshot.data.first;
+                      }
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              if (club?.clubLogoURL != null)
+                                ProfileImageViewer(
+                                  imagePath: club!.clubLogoURL,
+                                  enabled: false,
+                                  height: 20,
+                                ),
+                              Text(
+                                club?.name ?? "Club",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ].separate(6),
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Text(
+                                "Position: ",
+                                style: Theme.of(context).textTheme.labelLarge,
+                              ),
+                              Text(
+                                clubPosition?.position ?? "Position",
+                                style: Theme.of(context).textTheme.labelLarge,
+                              ),
+                            ],
+                          )
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+              StreamBuilder<List<UserModel>>(
+                stream: UserController.get(id: widget.receiverID),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<UserModel>> snapshot) {
+                  String receiverName = "Receiver";
+                  if (snapshot.hasData &&
+                      snapshot.data != null &&
+                      snapshot.data!.isNotEmpty) {
+                    receiverName = snapshot.data!.first.name;
+                  }
+                  return Row(
+                    children: [
+                      Text(
+                        "Receiver: ",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                      Text(
+                        receiverName.split(" ").first,
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.35,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () async {
+                await InvitationController.delete(widget.invitation);
+                widget.refresh();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text(
+                "Cancel request",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 11),
+              ),
+            ),
+          ),
+          // Row(
+          //   children: [
+          //     Tooltip(
+          //       message: "Accept",
+          //       child: IconButton(
+          //         onPressed: () async {
+          //           // Handle accept invitation
+          //           showLoadingOverlay(
+          //             context: context,
+          //             asyncTask: () async {
+          //               await InvitationController.acceptInvitation(
+          //                 invitation.id!,
+          //               );
+          //               onCompleteAcceptOrReject();
+          //               showSnackBar(context, "Welcome to the club");
+          //             },
+          //           );
+          //         },
+          //         icon: const Icon(
+          //           Icons.check,
+          //           color: Colors.green,
+          //         ),
+          //       ),
+          //     ),
+          //     Tooltip(
+          //       message: "Reject",
+          //       child: IconButton(
+          //         onPressed: () async {
+          //           // Handle reject invitation
+          //           await InvitationController.rejectInvitation(invitation);
+          //         },
+          //         icon: const Icon(
+          //           Icons.close,
+          //           color: Colors.red,
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+        ],
+      ),
+    );
+  }
+}

bce96caa8586f8dc188a94b3a7b162441e17b25c
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2024-02-19 04:30:56 +0530

    Fixed renderbox exceeding prob for invite item

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,5 +2,7 @@
 export 'club/club_model.dart';
 export 'club_position/club_position_model.dart';
 export 'event/event_model.dart';
+export 'institution/institution_model.dart';
+export 'invitation/invitaion_model.dart';
 export 'user/user_model.dart';
 export 'utils/utils.dart';
